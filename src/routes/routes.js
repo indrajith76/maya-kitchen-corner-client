de38710093e40c931ae129e125b1c20d0c53dc84
indrajith76 <indrajithgoswami@gmail.com>
2022-11-08 19:01:26 +0600

    configure navber and footer

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
